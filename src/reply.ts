b5232a56d7e19c06e04db75d4467a43284dfb717
anhkhoa55nn <khoa210105@gmail.com>
2025-03-30 15:40:45 +0700

    Update

--- src/reply.ts
+++ src/reply.ts
@@ -2,6 +2,9 @@ import { Scraper, SearchMode } from 'agent-twitter-client';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { askGemini } from './ask';
 import dotenv from "dotenv";
+import fs from "fs"
+import { log } from 'console';
+import { stringify } from 'querystring';
 
 dotenv.config();
 
@@ -20,7 +23,19 @@ export const replyTweet = async function handle(genAI: GoogleGenerativeAI, scrap
             SearchMode.Latest
         )
     ).tweets;
-    replyTweets.map(async (tweet: any) => {
+
+    let toReply = replyTweets;
+    if (fs.existsSync("replied.json")) {
+        const respone = fs.readFileSync("replied.json", 'utf-8')
+        const replied = JSON.parse(respone);
+
+        toReply = replyTweets.filter(item1 => {
+            const respone = replied.some((item2: any) => item1.id == item2.id)
+            return !respone;
+        })
+    }
+
+    toReply.map(async (tweet: any) => {
 
         //Reply
         const replyID = tweet.id;
@@ -31,6 +46,7 @@ export const replyTweet = async function handle(genAI: GoogleGenerativeAI, scrap
         const target = await scraper.getTweet(targetId);
         const targetText = target?.text;
         const contentToReply = await askGemini(genAI, "reply", targetText, replyContent);
+
         try {
             const response = await scraper.sendTweet(contentToReply, replyID);
         } catch (error) {
@@ -38,4 +54,7 @@ export const replyTweet = async function handle(genAI: GoogleGenerativeAI, scrap
         }
 
     })
+
+    const respone = fs.writeFileSync("replied.json", JSON.stringify(replyTweets))
+    
 }
